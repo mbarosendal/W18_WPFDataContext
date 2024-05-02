a5305af2f2f5f5e8230d091d7ccf2af054bb6afc
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2024-05-02 15:06:32 +0200

    Add project files.

---
+++ MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAndMVVM1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+		private string myLabelText = "Text not set yet";
+		public string MyLabelText
+		{
+			get { return myLabelText; }
+			set 
+			{ 
+				myLabelText = value;
+                // Alternativt skriv OnPropertyChanged(MyLabelText);
+                OnPropertyChanged(nameof(MyLabelText));
+			}
+		}		
+
+        private string myTextBoxText = "Text not set yet";
+        public string MyTextBoxText
+		{
+			get { return myTextBoxText; }
+			set 
+            { 
+                myTextBoxText = value;
+                OnPropertyChanged(nameof(MyTextBoxText));
+            }
+		}
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
